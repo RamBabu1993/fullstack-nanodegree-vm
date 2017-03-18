df0aeef3b59305f6a97d541e23657d46e929a705
Ram Babu <Ram Babu>
2017-03-18 22:09:13 +0530

    update tournament with code

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,43 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- drop database if exist in project
+   drop database if exists tournament;
+   
+-- create database tournament
+   
+   create database tournament;
+
+   \c tournament;
+
+   create table players(id serial primary key, name text);
+
+-- create table matches
+
+   create table matches(id serial primary key, 
+   	                    winner_id int references players(id),
+   	                    loser_id int references players(id));
+
+-- create view for winners record of matches
+
+   create view winners_record 
+   as select players.id, count(matches.winner_id)
+   from players left join matches
+   group by players.id;
+
+-- create view for losers record of matches
+
+   create view losers_record 
+   as select players.id, count(matches.loser_id)
+   from players left join matches
+   group by players.id;
+
+-- create view to record wins, losses and total matches played by a players
+
+   create view players_matches_record 
+   as select *
+   from winners_record natural join losers_record;
+   
+   
+
 
